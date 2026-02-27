8711ce4ebc9af59b7c6698b1d9f1596e7b118c0c
Harsh Tiwari <tiwari.harsh.p@gmail.com>
2026-02-27 15:10:30 +0530

    created server on port 8080

---
+++ server/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+//common middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+export default app;
